a83a7c310edaa1ba84314ffb2a87c96f8debf662
Do Vinh Dat <dovinhdat@Dos-MacBook-Pro.local>
2026-05-24 03:39:08 +0700

    enable sqlite migrate

--- Program.cs
+++ Program.cs
@@ -124,6 +124,10 @@ app.MapControllerRoute(
     name: "default",
     pattern:
         "{controller=Home}/{action=Index}/{id?}");
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 // RUN
 app.Run();
