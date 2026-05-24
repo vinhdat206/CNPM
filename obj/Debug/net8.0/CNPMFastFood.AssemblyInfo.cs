b1a60cc7940b838b2a369c0e2e138f522f50c8c5
Do Vinh Dat <dovinhdat@192.168.2.107>
2026-05-25 00:13:05 +0700

    Require login before adding product to cart

--- obj/Debug/net8.0/CNPMFastFood.AssemblyInfo.cs
+++ obj/Debug/net8.0/CNPMFastFood.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNPMFastFood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f9cbb0adf952d15b9d3693f8a8df0a4871b5b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f66f3b11df71eb28435bd0c10a770bf93e157ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNPMFastFood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNPMFastFood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
